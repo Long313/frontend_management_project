1d0b6053ab684209b50817874e9e9857b94fd9a1
Tran Xuan Long <tranxuanlonga555@gmail.com>
2023-11-22 08:31:16 +0700

    add reset css and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div className="bg-blue-600">test</div>;
+  return (
+    <Router>
+      <div className="App bg-blue-600">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
